6f9215a6041f029e00fae51936937624b656e119
Tristan Crispijn <Tristan@MacBook-Pro.local>
2016-12-28 05:36:31 +0100

    .

--- arduino/mysensors/vento2/vento2.ino
+++ arduino/mysensors/vento2/vento2.ino
@@ -1,4 +1,4 @@
-// #define MY_DEBUG
+#define MY_DEBUG
 #define MY_RADIO_NRF24
 #define MY_REPEATER_FEATURE
 #include <MySensors.h>
@@ -7,13 +7,20 @@
 #define RELAY_ON 0  
 #define RELAY_OFF 1 
 
-int state[NUMBER_OF_RELAYS];
+// +1 for easy offset state[0] not used
+int state[NUMBER_OF_RELAYS+1];
 
 void before() { 
   for (int sensor = 1, pin = RELAY_1; sensor <= NUMBER_OF_RELAYS; sensor++, pin++) {
     pinMode(pin, OUTPUT);
-    state[sensor] = false; //loadState(sensor);
-    digitalWrite(pin, state[sensor] ? RELAY_ON : RELAY_OFF);
+    digitalWrite(pin, RELAY_OFF);
+    state[sensor] = loadState(sensor);
+  }
+  digitalWrite(RELAY_1, state[1] ? RELAY_ON : RELAY_OFF);
+  if (state[1]) {
+    digitalWrite(RELAY_1 + 1, state[2] ? RELAY_ON : RELAY_OFF);
+    digitalWrite(RELAY_1 + 2, state[3] ? RELAY_ON : RELAY_OFF);
+    digitalWrite(RELAY_1 + 3, state[4] ? RELAY_ON : RELAY_OFF);   
   }
 }
 
@@ -35,42 +42,43 @@ void loop() {
 
 void go(int sensor,bool value) {
   if (sensor == 1) {
-    if (not value && state[2]) digitalWrite(2 + RELAY_1, RELAY_OFF);
-    if (not value && state[3]) digitalWrite(3 + RELAY_1, RELAY_OFF);
-    if (not value && state[4]) digitalWrite(4 + RELAY_1, RELAY_OFF);      
-    if (value && state[2]) digitalWrite(2 + RELAY_1, RELAY_ON);
-    if (value && state[3]) digitalWrite(3 + RELAY_1, RELAY_ON);
-    if (value && state[4]) digitalWrite(4 + RELAY_1, RELAY_ON);      
+    digitalWrite(RELAY_1, value ? RELAY_ON : RELAY_OFF);
+    if (not value && state[2]) digitalWrite(1 + RELAY_1, RELAY_OFF);
+    if (not value && state[3]) digitalWrite(2 + RELAY_1, RELAY_OFF);
+    if (not value && state[4]) digitalWrite(3 + RELAY_1, RELAY_OFF);      
+    if (value && state[2]) digitalWrite(1 + RELAY_1, RELAY_ON);
+    if (value && state[3]) digitalWrite(2 + RELAY_1, RELAY_ON);
+    if (value && state[4]) digitalWrite(3 + RELAY_1, RELAY_ON);      
   } else {
     if (state[1]) {
         digitalWrite(sensor - 1 + RELAY_1, value ? RELAY_ON : RELAY_OFF);
-    }  
+    } 
   }
   send(MyMessage(sensor, S_BINARY).set(value==HIGH));  
   state[sensor] = value;
   saveState(sensor,value);
+  Serial.println(state[sensor]);
 }
 
 void receive(const MyMessage &message) {
   if (message.type == V_STATUS) {
     if (message.sensor == 1) {
       go(1,message.getBool()); 
-    } else {
-      if (message.sensor == 2) {
-        go(3,false);
-        go(4,false);   
-        go(2,true);
-      }
-      if (message.sensor == 3) {
-        go(2,false);
-        go(4,false);
-        go(3,true);
-      }
-      if (message.sensor == 4) {
-        go(2,false);
-        go(3,false);
-        go(4,true);
-      }
+    }
+    if (message.sensor == 2) {
+      go(3,false);
+      go(4,false);   
+      go(2,true);
+    }
+    if (message.sensor == 3) {
+      go(2,false);
+      go(4,false);
+      go(3,true);
+    }
+    if (message.sensor == 4) {
+      go(2,false);
+      go(3,false);
+      go(4,true);
     }
 
     #ifdef MY_DEBUG
